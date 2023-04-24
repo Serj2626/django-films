dc824c41a54263e56c47ad6451b995bb5bf2dfad
Sergey Boytsov <serj2626@mail.ru>
2023-04-24 20:15:02 +0300

    add new models: film direction genre

--- films/models.py
+++ films/models.py
@@ -1,12 +1,45 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField('Название жанра', max_length=50)
+    description = models.TextField('Описание', blank=True)
+    url = models.SlugField(max_length=50, queue=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Direction(models.Model):
+    name = models.CharField('Фамилия и имя', max_length=150)
+    description = models.TextField('Биография', blank=True)
+    image = models.ImageField('Фотография', upload_to='image/directions/%Y')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Режиссер'
+        verbose_name_plural = 'Режиссеры'
+
+
 class Film(models.Model):
     '''Информация о фильме'''
-    title = models.CharField(max_length=100)
-    imag = models.ImageField(upload_to='image/%Y')
-    description = models.TextField(blank=True)
+    title = models.CharField('Название фильма', max_length=100)
+    image = models.ImageField('Постер', upload_to='image/%Y')
+    description = models.TextField('Описание', blank=True)
     date_publ = models.DateField('Дата выхода')
+    directions = models.ManyToManyField(Direction, verbose_name='Режиссер')
+    genre = models.ManyToManyField(Genre, verbose_name='Жанр')
+    url = models.SlugField(max_length=100, unique=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
