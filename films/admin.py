feae25315c1258db7942d7d9af6e9c1f5ecfaa00
Sergey Boytsov <serj2626@mail.ru>
2023-04-24 21:18:33 +0300

    add new text in db

--- films/admin.py
+++ films/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Film
+from .models import Film, Genre, Direction
 
 admin.site.register(Film)
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Direction)
--- films/models.py
+++ films/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField('Название жанра', max_length=50)
     description = models.TextField('Описание', blank=True)
-    url = models.SlugField(max_length=50, queue=True)
+    url = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
