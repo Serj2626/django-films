c6bf3be325188d423c26f1f0f4f2345fbcd7a262
Sergey Boytsov <serj2626@mail.ru>
2023-04-25 00:56:19 +0300

    update template film.html

--- Mysite/urls.py
+++ Mysite/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', admin.site.urls),
+    path('', include('films.urls')),
 ]
 
 if settings.DEBUG:
